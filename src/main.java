5b92c2c56110fea9e8e33142068b6f726e39e678
miyodeskun <miyodeskun@gmail.com>
2021-06-12 16:36:25 +0800

    Update toward intersection method and made few change at some parts

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
         System.out.print("{");
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n1; j++)
-                System.out.print("(" + arr1[i] + ", " + arr2[j] + "), ");
+                System.out.print("(" + arr1[i] + ", " + arr2[j] + "),");
         System.out.println("}");
     }
 
@@ -17,7 +17,7 @@ public class main {
 
         //Instruction Message
         System.out.println("INSTRUCTION!");
-        System.out.println("You need to create 2 different sets in order to get the relation.");
+        System.out.println("You need to create 4 different sets in order to get 2 relations.");
 
         // First Set
         System.out.print("\nEnter the number of elements for 1st Set: ");
@@ -37,8 +37,25 @@ public class main {
             arr2[i] = scan.nextInt(); //Reading array elements from the user
         }
 
-        int n1 = arr1.length;
-        int n2 = arr2.length;
+        // Third Set
+        System.out.print("Enter the number of elements for 3rd Set: ");
+        int o = scan.nextInt();
+        int arr3[] = new int[o];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < o; i++){
+            arr3[i]=scan.nextInt(); //Reading array elements from the user
+        }
+
+        // Fourth Set
+        System.out.print("Enter the number of elements for 4th Set: ");
+        int p = scan.nextInt();
+        int arr4[] = new int[p];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < p; i++) {
+            arr4[i] = scan.nextInt(); //Reading array elements from the user
+        }
+
+        int n1 = arr1.length, n2 = arr2.length, n3 = arr3.length, n4 = arr4.length;
 
         int op = 0;
         do {
@@ -54,22 +71,28 @@ public class main {
             } else if (op == 1) {
                 System.out.println("The Relation Statement");
                 System.out.print("Relation 1: ");
-                findRelation(arr1, arr2, n1, n2);
-                System.out.println("");
+                findRelation(arr1, arr2, n1, n2); // Display Relation 1
+                System.out.print("Relation 2: ");
+                findRelation(arr3, arr4, n3, n4); // Display Relation 2
                 op = 0;
             } else if (op == 2) {
                 System.out.println("Both relations no complementary because it already a complete relation.");
                 op = 0;
             } else if (op == 3) {
+                System.out.print("Inverse R1: ");
                 Inverse(arr1, arr2, n,n1);
+                System.out.print("Inverse R2: ");
+                Inverse(arr3, arr4, n3, n4);
                 op = 0;
             } else if (op == 4) {
                 System.out.println("The Union Relation");
                 Union();
                 op = 0;
             } else if (op == 5) {
-                System.out.println("The Intersection Relation");
-                Intersection();
+                System.out.print("Intersection R1 & R2= {");
+                Intersection(arr1, arr3, arr2, arr4);
+                System.out.print("}");
+                System.out.println("\nIf there is no answer, there is no intersection.");
                 op = 0;
             }
             else {
@@ -78,91 +101,26 @@ public class main {
         }while(op > 5 || op == 0);
     }
 
-    private static void Inverse(int arr1[], int arr2[],
-                                int n, int n1) {
-        System.out.print("Inverse R1 = {");
-        for (int i = 0; i < n; i++)
+    private static void Inverse(int arr1[], int arr2[], int n, int n1) {
+        System.out.print("{");
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n1; j++)
-                System.out.print("(" + arr2[j] + ", " + arr1[i] + "), ");
-        System.out.println("}");
+                System.out.print("(" + arr2[j] + ", " + arr1[i] + "),");
+        } System.out.println("}");
+
     }
 
-    static void Union(){
+    static void Union() {
 
-        // Create the first set
-        Set<Integer> set1 = new HashSet<>();
-        System.out.println("Total number in set 1 : ");
-        int tnum= scan.nextInt();
-        System.out.println("Enter Set 1 numbers: ");
-        for (int i = 0; i < tnum; i++) {
-            Integer a = scan.nextInt();
-            // Integer b = scan.nextInt();
-
-            set1.addAll(Collections.singleton(a));
-
-        }
-
-
-        System.out.println("Set1: " + set1);
-
-        // create second set
-        Set<Integer> set2 = new HashSet<>();
-        System.out.println("Total number in set 2 : ");
-        int tnum2= scan.nextInt();
-        System.out.println("Enter Set 2 numbers: ");
-
-        for (int i = 0; i < tnum2; i++) {
-            Integer b = scan.nextInt();
-            // Integer b = scan.nextInt();
-
-            set2.addAll(Collections.singleton(b));
-
-        }
-
-        System.out.println("Set2: " + set2);
-        System.out.println("\n Set 1 "+set1+" + Set 2: " + set2);
-
-
-        // Union of two sets
-        set2.addAll(set1);
-        System.out.println("Union is: " + set2);
     }
 
-    static void Intersection(){
-        // create first set
-        Set<Integer> set1 = new HashSet<>();
-        System.out.println("Total number in set 1 : ");
-        int tnum= scan.nextInt();
-        System.out.println("Enter Set 1 numbers: ");
+    public static void Intersection(int[] arr1, int[] arr3, int[] arr2, int[] arr4) {
 
-        for (int i = 0; i < tnum; i++) {
-            Integer a = scan.nextInt();
-            // Integer b = scan.nextInt();
-
-            set1.addAll(Collections.singleton(a));
-
-        }
-        System.out.println("Set 1: " + set1);
-
-        // create second set
-        Set<Integer> set2 = new HashSet<>();
-        System.out.println("Total number in set 2 : ");
-        int tnum2= scan.nextInt();
-        System.out.println("Enter Set 2 numbers: ");
-
-        for (int i = 0; i < tnum2; i++) {
-            Integer b = scan.nextInt();
-            // Integer b = scan.nextInt();
-
-            set2.addAll(Collections.singleton(b));
-
-        }
-        System.out.println("Set 2: " + set1+"\n");
-
-        System.out.println("Set 1 "+set1+" + Set 2: " + set2);
-
-        // Intersection of two sets
-        set2.retainAll(set1);
-        System.out.println("Intersection: " + set2);
+        for (int i = 0; i < arr1.length; i++)
+            for (int j = 0; j < arr3.length; j++)
+                for (int k = 0; k < arr2.length; k++)
+                    for (int l = 0; l < arr4.length; l++)
+                        if (arr1[i] == arr3[j] && arr2[k] == arr4[l])
+                            System.out.print("(" + arr1[i] + ", " + arr2[k] + "),");
     }
 }
